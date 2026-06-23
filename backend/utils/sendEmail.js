ec77d376ab1c6600cfc1019cacd9625219969cb6
Yash <y.yashsinghal207@gmail.com>
2026-06-23 22:46:25 +0530

    Fix SMTP connection timeout for faster Vercel responses

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,6 +3,9 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
+    connectionTimeout: 5000, // 5 seconds connection timeout
+    greetingTimeout: 5000,
+    socketTimeout: 5000,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
