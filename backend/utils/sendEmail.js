73bbd52e18c8ad9b590ffed097779924a8e4bdcc
Yash <y.yashsinghal207@gmail.com>
2026-06-23 22:50:24 +0530

    Configure Gmail SMTP to force IPv4 connection and port 587

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,14 +1,21 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // STARTTLS
     connectionTimeout: 5000, // 5 seconds connection timeout
     greetingTimeout: 5000,
     socketTimeout: 5000,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
+    },
+    // Force IPv4 resolution to prevent ENETUNREACH over IPv6 on Render
+    lookup: (hostname, options, callback) => {
+      dns.lookup(hostname, { family: 4 }, callback);
     },
   });
 
